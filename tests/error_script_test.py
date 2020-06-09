3a6639d930862987af51bebe9814f878a0434b35
Travis CI User <travis@example.org>
2020-06-09 21:31:19 +0000

    Fix pre-commit
    Signed-off-by: Travis CI User <travis@example.org>

--- tests/error_script_test.py
+++ tests/error_script_test.py
@@ -3,13 +3,14 @@
 """Test the rest interface."""
 import os
 from os.path import join, dirname
+from importlib import reload
 from time import sleep
 from multiprocessing import Process
 from cherrypy.test import helper
+import celery_setup_test
 
 os.environ['SCRIPT_DIR'] = os.path.join(os.path.dirname(__file__), 'script_dirs', 'error_script')
-
-from .celery_setup_test import TestDispatcherK8SBase
+reload(celery_setup_test)
 
 
 def _cli_entrypoint(*argv):
@@ -26,15 +27,17 @@ def _call_cli(*argv):
     return cli_proc.exitcode
 
 
-class DispatcherK8SErrorTest(TestDispatcherK8SBase, helper.CPWebCase):
+class DispatcherK8SErrorTest(celery_setup_test.TestDispatcherK8SBase, helper.CPWebCase):
     """Base class for all testing classes."""
 
     def test_error_script_path(self):
         """Test a default summation in example."""
+        # pylint: disable=import-outside-toplevel
         from pacifica.dispatcher_k8s.orm import ScriptLog
         return_code = _call_cli('pacifica-cli', 'upload', '--logon', 'dmlb2001', 'uploader.json')
         self.assertEqual(return_code, 0, 'return code wasn\'t zero {}'.format(return_code))
         sleep(10)
         scriptlog = ScriptLog.get()
-        self.assertTrue('This is an error string' in scriptlog.stderr, 'stderr did not contain \'This is an error string\'')
+        self.assertTrue('This is an error string' in scriptlog.stderr,
+                        'stderr did not contain \'This is an error string\'')
         self.assertEqual(scriptlog.return_code, '255', 'Script should error with -1 (255 unsigned byte)')
--- tests/script_test.py
+++ tests/script_test.py
@@ -3,13 +3,14 @@
 """Test the rest interface."""
 import os
 from os.path import join, dirname
+from importlib import reload
 from time import sleep
 from multiprocessing import Process
 from cherrypy.test import helper
+import celery_setup_test
 
 os.environ['SCRIPT_DIR'] = os.path.join(os.path.dirname(__file__), '..', 'contrib', 'example', 'scripts')
-
-from .celery_setup_test import TestDispatcherK8SBase
+reload(celery_setup_test)
 
 
 def _cli_entrypoint(*argv):
@@ -26,11 +27,12 @@ def _call_cli(*argv):
     return cli_proc.exitcode
 
 
-class DispatcherK8STest(TestDispatcherK8SBase, helper.CPWebCase):
+class DispatcherK8STest(celery_setup_test.TestDispatcherK8SBase, helper.CPWebCase):
     """Base class for all testing classes."""
 
     def test_happy_path(self):
         """Test a default summation in example."""
+        # pylint: disable=import-outside-toplevel
         from pacifica.dispatcher_k8s.orm import ScriptLog
         return_code = _call_cli('pacifica-cli', 'upload', '--logon', 'dmlb2001', 'uploader.json')
         self.assertEqual(return_code, 0, 'return code wasn\'t zero {}'.format(return_code))
